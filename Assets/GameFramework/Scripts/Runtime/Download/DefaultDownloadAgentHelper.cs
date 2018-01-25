835112bad100525e8cb34d0b229a7717dc8adb12
dianfs998 <yy.yanjl@gmail.com>
2018-01-25 20:59:37 +0800

    完善Download

---
+++ Assets/GameFramework/Scripts/Runtime/Download/DefaultDownloadAgentHelper.cs
@@ -0,0 +1,78 @@
+﻿using GameFramework;
+using GameFramework.Download;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 默认下载代理辅助器
+    /// </summary>
+    public class DefaultDownloadAgentHelper : DownloadAgentHelperBase, IDisposable
+    {
+        private WWW m_WWW = null;
+        private int m_LastDownloadedSize = 0;
+        private bool m_Disposed = false;
+
+        private event EventHandler<DownloadAgentHelperUpdateEventArgs> m_DownloadAgentHelperUpdateEventHandler = null;
+        private event EventHandler<DownloadAgentHelperCompleteEventArgs> m_DownloadAgentHelperCompleteEventHandler = null;
+        private event EventHandler<DownloadAgentHelperErrorEventArgs> m_DownloadAgentHelperErrorEventHandler = null;
+
+        /// <summary>
+        /// 下载代理辅助器更新事件
+        /// </summary>
+        public override event EventHandler<DownloadAgentHelperUpdateEventArgs> DownloadAgentHelperUpdate
+        {
+            add { m_DownloadAgentHelperUpdateEventHandler += value; }
+            remove { m_DownloadAgentHelperUpdateEventHandler -= value; }
+        }
+
+        /// <summary>
+        /// 下载代理辅助器完成事件
+        /// </summary>
+        public override event EventHandler<DownloadAgentHelperCompleteEventArgs> DownloadAgentHelperComplete
+        {
+            add { m_DownloadAgentHelperCompleteEventHandler += value; }
+            remove { m_DownloadAgentHelperCompleteEventHandler -= value; }
+        }
+
+        /// <summary>
+        /// 下载代理辅助器错误事件
+        /// </summary>
+        public override event EventHandler<DownloadAgentHelperErrorEventArgs> DownloadAgentHelperError
+        {
+            add { m_DownloadAgentHelperErrorEventHandler += value; }
+            remove { m_DownloadAgentHelperErrorEventHandler -= value; }
+        }
+
+        /// <summary>
+        /// 通过下载代理辅助器下载指定地址的数据
+        /// </summary>
+        /// <param name="downloadUri">下载地址</param>
+        /// <param name="userData">用户自定义数据</param>
+        public override void Download(string downloadUri, object userData)
+        {
+            if(m_DownloadAgentHelperUpdateEventHandler == null || m_DownloadAgentHelperCompleteEventHandler == null || m_DownloadAgentHelperErrorEventHandler == null)
+            {
+                Log.Fatal("Download agent helper handler is invalid.");
+                return;
+            }
+
+            m_WWW = new WWW(downloadUri);
+        }
+
+        public override void Download(string downloadUri, int fromPosition, object userData)
+        {
+            if (m_DownloadAgentHelperUpdateEventHandler == null || m_DownloadAgentHelperCompleteEventHandler == null || m_DownloadAgentHelperErrorEventHandler == null)
+            {
+                Log.Fatal("Download agent helper handler is invalid.");
+                return;
+            }
+
+            Dictionary<string, string> header = new Dictionary<string, string>();
+            header.Add("Range", string.Format("bytes={0}-", fromPosition.ToString()));
+            m_WWW = new WWW(downloadUri, null, header);
+        }
+    }
+}
--- Assets/GameFramework/Scripts/Runtime/Download/DownloadComponent.cs
+++ Assets/GameFramework/Scripts/Runtime/Download/DownloadComponent.cs
@@ -90,6 +90,9 @@ namespace UnityGameFramework.Runtime
             get { return m_DownloadManager.CurrentSpeed; }
         }
 
+        /// <summary>
+        /// 游戏框架组件初始化
+        /// </summary>
         protected override void Awake()
         {
             base.Awake();
@@ -103,6 +106,93 @@ namespace UnityGameFramework.Runtime
 
             m_DownloadManager.DownloadStart += OnDownloadStart;
             m_DownloadManager.DownloadUpdate += OnDownloadUpdate;
+            m_DownloadManager.DownloadSuccess += OnDownloadSuccess;
+            m_DownloadManager.DownloadFailure += OnDownloadFailure;
+            m_DownloadManager.FlushSize = m_FlushSize;
+            m_DownloadManager.Timeout = m_Timeout;
+        }
+
+        private void Start()
+        {
+            m_EventComponent = GameEntry.GetComponent<EventComponent>();
+            if(m_EventComponent == null)
+            {
+                Log.Fatal("Event component is invalid.");
+                return;
+            }
+
+            if(m_InstanceRoot == null)
+            {
+                m_InstanceRoot = (new GameObject("Download Agent Instances")).transform;
+                m_InstanceRoot.SetParent(gameObject.transform);
+                m_InstanceRoot.localScale = Vector3.one;
+            }
+
+            for(int i=0; i<m_DownloadAgentHelperCount; i++)
+            {
+                AddDownloadAgentHelper(i);
+            }
+        }
+
+        /// <summary>
+        /// 增加下载任务
+        /// </summary>
+        /// <param name="downloadPath">下载后的存放路径</param>
+        /// <param name="downloadUri">原始下载地址</param>
+        /// <returns>新增下载任务的序列编号</returns>
+        public int AddDownload(string downloadPath, string downloadUri)
+        {
+            return m_DownloadManager.AddDownload(downloadPath, downloadUri);
+        }
+
+        /// <summary>
+        /// 增加下载任务
+        /// </summary>
+        /// <param name="downloadPath">下载后的存放路径</param>
+        /// <param name="downloadUri">原始下载地址</param>
+        /// <param name="userData">用户自定义数据</param>
+        /// <returns>新增下载任务的序列编号</returns>
+        public int AddDownload(string downloadPath, string downloadUri, object userData)
+        {
+            return m_DownloadManager.AddDownload(downloadPath, downloadUri, userData);
+        }
+
+        /// <summary>
+        /// 移除下载任务
+        /// </summary>
+        /// <param name="serialId">下载任务的序列编号</param>
+        public void RemoveDownload(int serialId)
+        {
+            m_DownloadManager.RemoveDownload(serialId);
+        }
+
+        /// <summary>
+        /// 移除所有下载任务
+        /// </summary>
+        public void RemoveAllDownload()
+        {
+            m_DownloadManager.RemoveAllDownload();
+        }
+
+        /// <summary>
+        /// 增加下载代理辅助器
+        /// </summary>
+        /// <param name="index">下载代理辅助器索引</param>
+        private void AddDownloadAgentHelper(int index)
+        {
+            DownloadAgentHelperBase downloadAgentHelper = Helper.CreateHelper(m_DownloadAgentHelperTypeName, m_CustomDownloadAgentHelper, index);
+            if(downloadAgentHelper == null)
+            {
+                Log.Error("Can not create download agent helper.");
+                return;
+            }
+
+            downloadAgentHelper.name = string.Format("Download Agent Helper - {0}", index.ToString());
+            Transform transform = downloadAgentHelper.transform;
+            transform.SetParent(m_InstanceRoot);
+            transform.localScale = Vector3.one;
+
+            m_DownloadManager.AddDownloadAgentHelper(downloadAgentHelper);
         }
 
         private void OnDownloadStart(object sender, GameFramework.Download.DownloadStartEventArgs e)
@@ -113,6 +203,16 @@ namespace UnityGameFramework.Runtime
         private void OnDownloadUpdate(object sender, GameFramework.Download.DownloadUpdateEventArgs e)
         {
             m_EventComponent.Fire(this, ReferencePool.Acquire<DownloadUpdateEventArgs>().Fill(e));
+        }
+
+        private void OnDownloadSuccess(object sender, GameFramework.Download.DownloadSuccessEventArgs e)
+        {
+            m_EventComponent.Fire(this, ReferencePool.Acquire<DownloadSuccessEventArgs>().Fill(e));
+        }
+
+        private void OnDownloadFailure(object sender, GameFramework.Download.DownloadFailureEventArgs e)
+        {
+            m_EventComponent.Fire(this, ReferencePool.Acquire<DownloadFailureEventArgs>().Fill(e));
         }
     }
 }
