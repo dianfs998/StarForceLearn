9f5a8e501bf8a4743b515dd6649288e6cdf84daf
JL YAN <yy.yanjl@gmail.com>
2018-01-30 00:09:06 +0800

    完善Download，增加Entity，Fsm，Localization，Network，ObjectPool

---
+++ Assets/GameFramework/Scripts/Runtime/Entity/EntityComponent.EntityGroup.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+
+namespace UnityGameFramework.Runtime
+{
+    public partial class EntityComponent
+    {
+		[Serializable]
+		private sealed class EntityGroup
+        {
+            [SerializeField]
+            private string m_Name = null;
+
+            [SerializeField]
+            private float m_InstanceAutoReleaseInternal = 60f;
+
+            [SerializeField]
+            private int m_InstanceCapacity = 16;
+
+            [SerializeField]
+            private float m_InstanceExpireTime = 60f;
+
+            [SerializeField]
+            private int m_InstancePriority = 0;
+			
+            public string Name
+            {
+                get { return m_Name; }
+            }
+
+			public float InstanceAutoReleaseInterval
+            {
+                get { return m_InstanceAutoReleaseInternal; }
+            }
+
+			public int InstanceCapacity
+            {
+                get { return m_InstanceCapacity; }
+            }
+
+			public float InstanceExpireTime
+            {
+                get { return m_InstanceExpireTime; }
+            }
+
+			public int InstancePriority
+            {
+                get { return m_InstancePriority; }
+            }
+        }
+    }
+}
---
+++ Assets/GameFramework/Scripts/Runtime/ObjectPool/ObjectPoolComponent.cs
@@ -0,0 +1,66 @@
+﻿using GameFramework;
+using GameFramework.ObjectPool;
+using UnityEngine;
+using System;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 对象池组件
+    /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Game Framework/Object Pool")]
+    public sealed class ObjectPoolComponent : GameFrameworkComponent
+    {
+        private IObjectPoolManager m_ObjectPoolManager = null;
+
+        /// <summary>
+        /// 游戏框架组件初始化
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
+
+            m_ObjectPoolManager = GameFrameworkEntry.GetModule<IObjectPoolManager>();
+            if(m_ObjectPoolManager == null)
+            {
+                Log.Fatal("Object pool is invalid.");
+                return;
+            }
+        }
+
+        private void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// 获取对象池数量
+        /// </summary>
+        /// <returns></returns>
+        public int Count()
+        {
+            return m_ObjectPoolManager.Count;
+        }
+
+        /// <summary>
+        /// 检查是否存在对象池
+        /// </summary>
+        /// <typeparam name="T">对象池类型</typeparam>
+        /// <returns>是否存在对象池</returns>
+        public bool HasObjectPool<T>() where T : ObjectBase
+        {
+            return m_ObjectPoolManager.HasObjectPool<T>();
+        }
+
+        /// <summary>
+        /// 检查是否存在对象池
+        /// </summary>
+        /// <param name="objectType">对象池类型</param>
+        /// <returns>是否存在对象池</returns>
+        public bool HasObjectPool(Type objectType)
+        {
+            return m_ObjectPoolManager.HasObjectPool(objectType);
+        }
+    }
+}
