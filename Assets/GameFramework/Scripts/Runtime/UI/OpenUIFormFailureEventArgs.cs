99352c6400e0dbdf8634295557d3ccb91b6e5244
JL YAN <yy.yanjl@gmail.com>
2018-02-11 22:53:59 +0800

    完善UI

---
+++ Assets/GameFramework/Scripts/Runtime/UI/OpenUIFormFailureEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using GameFramework.Event;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 打开界面失败事件
+    /// </summary>
+    public sealed class OpenUIFormFailureEventArgs : GameEventArgs
+    {
+        /// <summary>
+        /// 打开界面失败事件编号
+        /// </summary>
+        private static readonly int EventId = typeof(OpenUIFormFailureEventArgs).GetHashCode();
+
+        /// <summary>
+        /// 获取打开界面失败事件
+        /// </summary>
+        public override int Id
+        {
+            get { return EventId; }
+        }
+
+        /// <summary>
+        /// 获取界面序列编号
+        /// </summary>
+        public int SerialId { get; private set; }
+
+        /// <summary>
+        /// 获取界面资源名称
+        /// </summary>
+        public string UIFormAssetName { get; private set; }
+
+        /// <summary>
+        /// 获取界面组名称
+        /// </summary>
+        public string UIGroupName { get; private set; }
+
+        /// <summary>
+        /// 获取是否暂停被覆盖的界面
+        /// </summary>
+        public bool PauseCoveredUIForm { get; private set; }
+
+        /// <summary>
+        /// 获取错误信息
+        /// </summary>
+        public string ErrorMessage { get; private set; }
+
+        /// <summary>
+        /// 获取用户自定义数据
+        /// </summary>
+        public object UserData { get; private set; }
+
+        public override void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
