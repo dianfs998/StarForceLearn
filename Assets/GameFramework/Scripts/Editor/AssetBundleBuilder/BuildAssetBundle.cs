5a7078fe9fe53655cea825e27d7bf39bb71eb6a2
dianfs998 <yy.yanjl@gmail.com>
2018-03-01 18:33:32 +0800

    添加AssetBundleBuilder

---
+++ Assets/GameFramework/Scripts/Editor/AssetBundleBuilder/BuildAssetBundle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace UnityGameFramework.Editor.AssetBundleTools
+{
+    /// <summary>
+    /// 生成资源包
+    /// </summary>
+    internal sealed class BuildAssetBundle
+    {
+        [MenuItem("Game Framework/AssetBundle Tools/Build AssetBundle", false, 30)]
+        private static void Run()
+        {
+            
+        }
+    }
+}
