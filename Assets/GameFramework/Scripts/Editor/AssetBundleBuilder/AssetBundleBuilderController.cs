4dabc6059939d0127b65242d117b47b6818143c2
JL YAN <yy.yanjl@gmail.com>
2018-03-01 23:33:56 +0800

    添加AssetBundleBuilder

---
+++ Assets/GameFramework/Scripts/Editor/AssetBundleBuilder/AssetBundleBuilderController.cs
@@ -0,0 +1,13 @@
+﻿using GameFramework;
+
+namespace UnityGameFramework.Editor.AssetBundleTools
+{
+    internal sealed partial class AssetBundleBuilderController : Utility.Zip.IZipHelper
+    {
+        private const string VersionListFileName = "version";
+        private const string ResourceListFileName = "list";
+        private const string RecordName = "GameResourceVersion";
+        private const string NoneOptionName = "<None>";
+        private static readonly char[] PackageListHeader = new char[] { 'E', 'L', 'P' };
+    }
+}
